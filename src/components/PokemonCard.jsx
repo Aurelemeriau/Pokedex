c3cf25644067cbbf0fab1ff44bec4e55e0ec131e
Aurelien <aurelien.emeriau@hotmail.fr>
2023-10-10 10:21:13 +0200

    quete 3 react

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,32 @@
 function PokemonCard () {
 
+  const pokemon = pokemonList[0];
+
+  if (pokemon.imgSrc === "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png") { 
+
    return <figure>
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
             <figcaption>Bulbasaur</figcaption>
           </figure>
-
+  } else {
+    return <figure>
+             <p>???</p>
+             <figcaption>Mew</figcaption>
+           </figure>
+    
+  }
 }
+
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
